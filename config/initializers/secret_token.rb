9573cd2e891fc2e38d99843bfe5e29d89e9e2ddb
Ben Paddock <pads@thisispads.me.uk>
2013-10-28 10:39:21 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DesignStagingApp::Application.config.secret_token = 'cb41e6f64d3cc151a107fc2deb6fd93e8529c9733a05b3c225cc4a14da8f3f4f3e10085fa0260afb1ea735c78b17f683a54b10cd34cf77babd2c35e7fe6f653d'
